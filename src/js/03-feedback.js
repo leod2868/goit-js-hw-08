6fe61c41f95e9b5c9bd2bb998f676f42dfc7b8a7
Leonid <ztvlnd777@gmail.com>
2023-03-11 20:04:07 +0200

    8/3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
 
+const refs = {
+  form : document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea : document.querySelector('.feedback-form textarea'),
+}
+const formData = {};
+const KEY = 'feedback-form-state'
+helloInput();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', e => {
+
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(KEY, JSON.stringify(formData))
+});
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    console.log(formData);
+    localStorage.removeItem(KEY);
+}
+
+function helloInput(e) {
+ const saveText =JSON.parse(localStorage.getItem(KEY))
+ 
+}
