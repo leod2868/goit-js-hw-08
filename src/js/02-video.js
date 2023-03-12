6b9f65a4ea8cb96c307d43f58690def7c6af3d39
Leonid <ztvlnd777@gmail.com>
2023-03-12 21:13:02 +0200

    new2/8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,17 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-const player = new Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (time) {
+    localStorage.setItem(STORAGE_KEY, time.seconds);
+};
+player.on('timeupdate', throttle((onPlay), 1000));
+
+function helloVideo(e) {
+    const saveVideo = localStorage.getItem(STORAGE_KEY);
+    if(saveVideo)  {}
+}
+
